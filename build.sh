0649d7d799ec030e635d69f7fd2d1add25d08373
pratik117-dev <pratikpoudel1110@gmail.com>
2025-11-04 22:06:57 +0545

    changed

--- build.sh
+++ build.sh
@@ -1,14 +1,11 @@
-set -o errexit 
+#!/usr/bin/env bash
+set -o errexit
 
 pip install -r requirements.txt
 
-
 python manage.py collectstatic --no-input
-
-
 python manage.py migrate
 
-if[[$CREATE_SUPERUSER]];
-then 
+if [ "$CREATE_SUPERUSER" = "True" ]; then
     python manage.py createsuperuser --no-input
 fi
