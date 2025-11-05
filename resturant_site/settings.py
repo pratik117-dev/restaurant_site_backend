11f82b4cd1371429a3173b968f5ae67348c2159b
pratik117-dev <pratikpoudel1110@gmail.com>
2025-11-05 14:24:17 +0545

    smtp allowded

--- resturant_site/settings.py
+++ resturant_site/settings.py
@@ -145,7 +145,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # Prints OTP to logs
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # Prints OTP to logs
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
