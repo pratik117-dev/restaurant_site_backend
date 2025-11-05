b4a0942a52b4307123ca0fae2149033ae303c7d2
pratik117-dev <pratikpoudel1110@gmail.com>
2025-11-05 14:45:43 +0545

    smtp activated

--- resturant_site/settings.py
+++ resturant_site/settings.py
@@ -145,13 +145,13 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # Prints OTP to logs
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ppoudel1011@gmail.com'  # Replace with your Gmail
-EMAIL_HOST_PASSWORD = 'mevi jkzk bfxx nnye'  # Use App Password for Gmail
-DEFAULT_FROM_EMAIL = 'ppoudel1011@gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'  # Use database for sessions
