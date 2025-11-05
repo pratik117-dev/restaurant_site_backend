1a4935f2141df46c39487e28af847179bf5dc283
pratik117-dev <pratikpoudel1110@gmail.com>
2025-11-05 09:06:17 +0545

    changed urls

--- resturant_site/urls.py
+++ resturant_site/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('orders.urls')),
+    path('api/', include('orders.urls')),
 ]
 
 # Serve media files in development
