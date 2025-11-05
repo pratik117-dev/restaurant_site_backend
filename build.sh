32965b2fa6953434732a311209a16ecfc9e195be
pratik117-dev <pratikpoudel1110@gmail.com>
2025-11-05 07:38:46 +0545

    changed build

--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-if [ "$CREATE_SUPERUSER" = "True" ]; then
-    python manage.py createsuperuser --no-input
-fi
+# if [ "$CREATE_SUPERUSER" = "True" ]; then
+#     python manage.py createsuperuser \
+#       --email "$DJANGO_SUPERUSER_EMAIL" \
+#       --name "$DJANGO_SUPERUSER_NAME" \
+#       --no-input
+# fi
+
