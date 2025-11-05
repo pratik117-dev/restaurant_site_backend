58c34bff904e0c972f848d318617caa44248dfb4
pratik117-dev <pratikpoudel1110@gmail.com>
2025-11-05 16:24:19 +0545

    changed

--- resturant_site/settings.py
+++ resturant_site/settings.py
@@ -147,7 +147,7 @@ CORS_ALLOWED_ORIGINS = [
 
 import os
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
