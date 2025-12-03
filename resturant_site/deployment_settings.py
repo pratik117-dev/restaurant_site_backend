c063a12e0b31f3c7f6d5a1bacd22f5ab92933925
pratik117-dev <pratikpoudel1110@gmail.com>
2025-12-03 12:37:05 +0545

    new

--- resturant_site/deployment_settings.py
+++ resturant_site/deployment_settings.py
@@ -1,17 +1,22 @@
-import os 
+"""
+Production settings for Render deployment.
+"""
+
+import os
 import dj_database_url
-from .settings import *
 from .settings import BASE_DIR
 
+# -------------------
+# SECURITY
+# -------------------
+DEBUG = False
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALLOWED_HOSTS = [os.environ.get("RENDER_EXTERNAL_HOSTNAME")]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get("RENDER_EXTERNAL_HOSTNAME")]
 
-ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
-CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
-
-DEBUG = True
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
-
-
+# -------------------
+# MIDDLEWARE
+# -------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -24,26 +29,43 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
+# -------------------
+# CORS
+# -------------------
 CORS_ALLOWED_ORIGINS = [
-    'https://restaurant-site-backend-frontend.onrender.com',  # Frontend URL
+    'https://restaurant-site-backend-frontend.onrender.com',
 ]
 
-
-
-STORAGES ={
-    "default": {
-        "BACKEND" : 'django.core.files.storage.FileSystemStorage'
-    },
-    "staticfiles": {
-        "BACKEND":"whitenoise.storage.CompressedStaticFilesStorage"
-    }
+# -------------------
+# STATIC & MEDIA
+# -------------------
+STORAGES = {
+    "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
+    "staticfiles": {"BACKEND": "whitenoise.storage.CompressedStaticFilesStorage"},
 }
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# -------------------
+# DATABASE (PostgreSQL)
+# -------------------
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL not set in Render environment")
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default= os.environ['DATABASE_URL'],
-        conn_max_age= 600
+    "default": dj_database_url.parse(
+        DATABASE_URL,
+        conn_max_age=600,
+        conn_health_checks=True,
     )
 }
+
+# -------------------
+# CUSTOM USER
+# -------------------
+AUTH_USER_MODEL = 'orders.CustomUser'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
