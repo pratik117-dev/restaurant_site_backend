a455c538e3df64da30097de841eaac771c1d54b8
pratik117-dev <pratikpoudel1110@gmail.com>
2025-12-05 12:35:08 +0545

    chnaged

--- resturant_site/deployment_settings.py
+++ resturant_site/deployment_settings.py
@@ -3,4 +3,19 @@ from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1","*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","*", "https://restaurantsitebackend-production.up.railway.app"]
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DATABASE_NAME"),
+        "USER": config("DATABASE_USER"),
+        "PASSWORD": config("DATABASE_PASSWORD"),
+        "HOST": config("DATABASE_HOST"),
+        "PORT": config("DATABASE_PORT", default="5432"),
+        "OPTIONS": {
+            "sslmode": "require",  # recommended for Railway / external Postgres
+        },
+    }
+}
